d7c2a3fc97d5db63e6bb8d660bc58bd37385f58c
Baha Celik <35celik.baha@gmail.com>
2023-04-18 18:59:17 +0300

    Update test_calculator.py

--- Calculator/test_calculator.py
+++ Calculator/test_calculator.py
@@ -1,34 +1,52 @@
-from calculator import Calculator
-def test_calculator():
-    # create a new calculator instance
-    calc = Calculator()
 
+from unittest import TestCase
+
+from Calculator.calculator import BahaCalculator
+
+
+class Testing(TestCase):
+    #JTL reccomendation divide the test
+    
+    
+    def test_add(self):
+        calc = BahaCalculator()
     # test addition
-    assert calc.add(2, 3) == 5
-    assert calc.add(-2, 3) == 1
-    assert calc.add(0.1, 0.2) == 0.3
-
+        assert calc.add(3) == 3
+        assert calc.add(3) == 6
+        
+    def test_subs(self):
+        calc = BahaCalculator()
     # test subtraction
-    assert calc.subtract(5, 2) == 3
-    assert calc.subtract(5, -2) == 7
-    assert calc.subtract(0.3, 0.2) == 0.1
+        assert calc.subtract(3) == -3
+        assert calc.subtract(2) == -5
+    
+        
+    def test_mult(self):
+        calc = BahaCalculator()
+        # test multiplication
+        calc.add(2)
+        assert calc.multiply(3) == 6
+        assert calc.multiply(10) == 60
+    
+    def test_division(self):
+        calc = BahaCalculator()
+        # test division
+        calc.add(30)
+        assert calc.divide(5) == 6
+        assert calc.divide(2) == 3
 
-    # test multiplication
-    assert calc.multiply(2, 3) == 6
-    assert calc.multiply(-2, 3) == -6
-    assert calc.multiply(0.1, 0.2) == 0.02
-
-    # test division
-    assert calc.divide(6, 3) == 2
-    assert calc.divide(-6, 3) == -2
-    assert calc.divide(0.2, 0.1) == 2
-
-    # test taking the nth root of a number
-    assert calc.root(4, 81) == 3
-    assert calc.root(3, 27) == 3
-    assert calc.root(2, 25) == 5
-
-    # test resetting the memory
-    calc.add(2, 3)
-    calc.reset()
-    assert calc.memory == 0
+    def test_root(self):
+        calc = BahaCalculator()
+        # test taking the nth root of a number
+        calc.add(36)
+        assert calc.root(1) == 36
+        assert calc.multiply(12) ==432
+        assert calc.root(3) == 7.559526299369238
+        
+    def test_reset(self):
+        calc = BahaCalculator()
+        # test resetting the memory
+        calc.add(3)
+        calc.reset()
+        assert calc.memory == 0
+    
